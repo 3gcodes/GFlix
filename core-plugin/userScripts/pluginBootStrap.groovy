8be528863257857d8cac145c52d071eae35bec19
gdboling <gdboling@gmail.com>
2011-05-04 15:16:08 -0500

    added bootstrap data for Movie

--- core-plugin/userScripts/pluginBootStrap.groovy
+++ core-plugin/userScripts/pluginBootStrap.groovy
@@ -1,3 +1,13 @@
 /**
  * This script is used to bootstrap data that is required for the core-plugin domain model
 **/
+new Movie(title:"The Karate Kid", description: "Danial kicks Johnny's ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"The Karate Kid, Part II", description: "Danial kicks Chozen's ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"The Karate Kid, Part III", description: "Danial kicks Terry's ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"Inception", description: "People play crazy mind games", releaseDate:new Date()).save(flush:true)
+new Movie(title:"Braveheart", description: "Scottish guy kicks English ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"300", description: "Spartans kick Persian ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"300", description: "Spartans kick Persian ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"300", description: "Spartans kick Persian ass", releaseDate:new Date()).save(flush:true)
+new Movie(title:"300", description: "Spartans kick Persian ass", releaseDate:new Date()).save(flush:true)
+
