ef418d197b557839917ebfda5c35caa1a6b5d330
gdboling <gdboling@gmail.com>
2011-05-04 14:57:57 -0500

    added properties and constraints to Movie

--- core-plugin/grails-app/domain/com/gflix/Movie.groovy
+++ core-plugin/grails-app/domain/com/gflix/Movie.groovy
@@ -1,7 +1,18 @@
 package com.gflix
 
 class Movie {
+	
+	String title
+	String description
+	Date releaseDate
+	Date dateCreated
+	Date lastUpdated
 
     static constraints = {
+		title nullable:false, blank:false, maxSize: 150
+		description nullable:true, maxSize: 4000
+		releaseDate nullable:false
+		dateCreated nullable:true
+		lastUpdated nullable:true
     }
 }
