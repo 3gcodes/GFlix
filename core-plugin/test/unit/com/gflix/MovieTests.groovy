0642def19c5bacee9958dc815361f220b5b8efc1
gdboling <gdboling@gmail.com>
2011-05-04 15:06:20 -0500

    added test for successful save to Movie

--- core-plugin/test/unit/com/gflix/MovieTests.groovy
+++ core-plugin/test/unit/com/gflix/MovieTests.groovy
@@ -14,10 +14,14 @@ class MovieTests extends GrailsUnitTestCase {
 
     void testNullables() {
 		def movieInstance = new Movie()
-		
 		assertFalse 'validation should fail because not-nullable properties are null', movieInstance.validate()
 		assertEquals 'title is required', 'nullable', movieInstance.errors.title
 		assertEquals 'releaseDate is required', 'nullable', movieInstance.errors.releaseDate
-
     }
+
+	void testSaveSuccessful() {
+		def movieInstance = new Movie(title:"The Karate Kid", description: "Danial kicks Johnny's ass", releaseDate:new Date())
+		movieInstance.save(flush:true)
+		assertNotNull 'movie instance has no ID', movieInstance.id
+	}
 }
