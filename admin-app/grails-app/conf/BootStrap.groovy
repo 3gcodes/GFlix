9dcd90ce38712bb18c17f89c6c806cdcad179fc4
gdboling <gdboling@gmail.com>
2011-05-04 15:17:23 -0500

    added bootstrap data for Movie

--- admin-app/grails-app/conf/BootStrap.groovy
+++ admin-app/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
 class BootStrap {
 
     def init = { servletContext ->
+		environments {
+			development {
+				new Movie(title:"The Karate Kid", description: "Danial kicks Johnny's ass", releaseDate:new Date()).save(flush:true)
+				new Movie(title:"The Karate Kid, Part II", description: "Danial kicks Chozen's ass", releaseDate:new Date()).save(flush:true)
+				new Movie(title:"The Karate Kid, Part III", description: "Danial kicks Terry's ass", releaseDate:new Date()).save(flush:true)
+				new Movie(title:"Braveheart", description: "Scottish guy kicks English ass", releaseDate:new Date()).save(flush:true)
+				new Movie(title:"300", description: "Spartans kick Persian ass", releaseDate:new Date()).save(flush:true)
+				new Movie(title:"Kick Ass", description: "Fake super hero gets his ass kicked", releaseDate:new Date()).save(flush:true)
+				new Movie(title:"Crouching Tiger, Hidden Dragon", description: "Yu Shu Lien kicks everyone's ass", releaseDate:new Date()).save(flush:true)
+			}
+		}
     }
     def destroy = {
     }
