8b70918f02af7729fd70df0cf7572e7181114d9f
gdboling <gdboling@gmail.com>
2011-05-04 15:03:07 -0500

    added nullable tests for Movie

--- core-plugin/test/unit/com/gflix/MovieTests.groovy
+++ core-plugin/test/unit/com/gflix/MovieTests.groovy
@@ -5,13 +5,19 @@ import grails.test.*
 class MovieTests extends GrailsUnitTestCase {
     protected void setUp() {
         super.setUp()
+		mockDomain(Movie)
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testNullables() {
+		def movieInstance = new Movie()
+		
+		assertFalse 'validation should fail because not-nullable properties are null', movieInstance.validate()
+		assertEquals 'title is required', 'nullable', movieInstance.errors.title
+		assertEquals 'releaseDate is required', 'nullable', movieInstance.errors.releaseDate
 
     }
 }
